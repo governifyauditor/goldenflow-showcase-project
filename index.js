7827843faaed2084cab8d76f0d056207edc3c176
Governify Auditor <67601636+governifyauditor@users.noreply.github.com>
2021-02-03 13:09:45 +0100

    feat: augmented coverage

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ console.log("Deployed");
 module.exports.returnThousand = () => {
     console.log("returnThousand");
     console.log("more lines!");
-    console.log("more lines!");
-    console.log("more lines!");
-    console.log("more lines!");
     return 1000;
 }
 
